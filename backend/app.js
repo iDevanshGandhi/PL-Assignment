ae6dab8df78359dd2bb280ffe0e6f9aa22f49a90
Devansh Gandhi <contactdevanshgandhi@gmail.com>
2023-02-25 19:04:58 +0530

    connected to database

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,8 @@
 import express from "express";
+import mongoose from 'mongoose'
 
 const app = express();
 
-app.use("/api", (req, res, next) => {
-    res.send("Hello World");
-});
-
-app.listen(5000);
+mongoose.connect(
+    'mongodb+srv://devanshgandhi:tieRyPPzn6PSCa8t@cluster0.as1bb3n.mongodb.net/Blog?retryWrites=true&w=majority'
+    ).then(()=>app.listen(5000)).then(()=>console.log("Connected TO Database and Listening TO Localhost 5000")).catch((err)=>console.log(err));
