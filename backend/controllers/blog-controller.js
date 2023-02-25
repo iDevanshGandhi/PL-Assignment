6ab210cd201158e1f0ce2537039736f7e2f2f530
Devansh Gandhi <contactdevanshgandhi@gmail.com>
2023-02-26 02:45:39 +0530

    almost done

--- backend/controllers/blog-controller.js
+++ backend/controllers/blog-controller.js
@@ -46,3 +46,33 @@ export const updateBlog = async (req, res, next)=> {
     }
     return res.status(200).json({ blog });
 }
+
+export const getById = async (req, res, next)=> {
+    // const {title,description} = req.body;
+    const id = req.params.id;
+    let blog;
+    try {
+        blog = await blog.findById(id);
+    } catch (err) {
+        return console.log(err);
+    }
+    if(!blog) {
+        return res.status(404).json({message: "No Blog Found"});
+    }
+    return res.status(200).json({ blog });
+};
+
+export const deleteBlog = async (req, res, next)=> {
+    // const {title,description} = req.body;
+    const id = req.params.id;
+    let blog;
+    try {
+        blog = await blog.findIdAndRemove(id);
+    } catch (err) {
+        return console.log(err);
+    }
+    if(!blog) {
+        return res.status(500).json({message: "Unable to Delete"});
+    }
+    return res.status(200).json({ message: "Deleted Successfully" });
+};
--- backend/routes/blog-routes.js
+++ backend/routes/blog-routes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getAllBlogs } from "../controllers/blog-controller";
+import { deleteBlog, getAllBlogs, getById, updateBlog } from "../controllers/blog-controller";
 import { addBlog } from "../controllers/blog-controller";
 const blogRouter = express.Router();
 
 blogRouter.get("/",getAllBlogs);
 blogRouter.post("/add",addBlog);
-blogRouter.post("/update/:id",updateBlog)
+blogRouter.put("/update/:id",updateBlog);
+blogRouter.get("/:id",getById);
+blogRouter.delete("/:id",deleteBlog);
 
 export default blogRouter;
