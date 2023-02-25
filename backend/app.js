7fbc1e5118e0afc86b100ed6415fb848e2085e39
Devansh Gandhi <contactdevanshgandhi@gmail.com>
2023-02-25 19:43:15 +0530

    checking the api call

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 import express from "express";
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
+import router from "./routes/user-routes";
+    
 
 const app = express();
 
+app.use("/api/user",router)
 mongoose.connect(
     'mongodb+srv://devanshgandhi:tieRyPPzn6PSCa8t@cluster0.as1bb3n.mongodb.net/Blog?retryWrites=true&w=majority'
     ).then(()=>app.listen(5000)).then(()=>console.log("Connected TO Database and Listening TO Localhost 5000")).catch((err)=>console.log(err));
---
+++ backend/model/user.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const Schema = new Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phoneno: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    }
+});
+
+export default mongoose.model("User", userSchema);
+// it will be stored by the name "users" in the mongodb database
